971846132c241dea88d8f95d97924de08a043758
Pascale <pascale.koop@live.com>
2021-07-08 10:56:25 +0200

    add  rotateSvg  function
    add rotate function to rotate the svg on click

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ window.addEventListener("DOMContentLoaded", event => {
     });
   });
 });
+
+let rotation = 0;
+const rotateSvg = () => {
+  rotation += 180; //add 180 degrees
+  if (rotation === 360) {
+    // 360 means rotate back to 0
+    rotation = 0;
+  }
+
+  document.querySelector("#svg").style.transform = `rotate(${rotation}deg)`;
+};
