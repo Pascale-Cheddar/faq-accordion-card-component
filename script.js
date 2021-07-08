96bbd3e345c22d96ba6f70fcd61fdbd372908d2f
Pascale <pascale.koop@live.com>
2021-07-08 12:21:43 +0200

    add rotation function differ names
    try to rotate each arrow by his own
    with one function it is not working.
    my solution is to add a function for each  button and his child svg  with different names but sematic

--- script.js
+++ script.js
@@ -28,3 +28,55 @@ const rotateSvg = () => {
 
   document.querySelector("#svg").style.transform = `rotate(${rotation}deg)`;
 };
+
+let secondRotation = 0;
+const rotateSvgSecond = () => {
+  secondRotation += 180; //add 180 degrees
+  if (secondRotation === 360) {
+    // 360 means rotate back to 0
+    SecondRotation = 0;
+  }
+
+  document.querySelector(
+    "#svg2"
+  ).style.transform = `rotate(${secondRotation}deg)`;
+};
+
+let thirdRotation = 0;
+const rotateSvgThird = () => {
+  thirdRotation += 180; //add 180 degrees
+  if (thirdRotation === 360) {
+    // 360 means rotate back to 0
+    thirdRotation = 0;
+  }
+
+  document.querySelector(
+    "#svg3"
+  ).style.transform = `rotate(${thirdRotation}deg)`;
+};
+
+let fourthRotation = 0;
+const rotateSvgFourth = () => {
+  fourthRotation += 180; //add 180 degrees
+  if (fourthRotation === 360) {
+    // 360 means rotate back to 0
+    fourthRotation = 0;
+  }
+
+  document.querySelector(
+    "#svg4"
+  ).style.transform = `rotate(${fourthRotation}deg)`;
+};
+
+let fifthRotation = 0;
+const rotateSvgFifth = () => {
+  fifthRotation += 180; //add 180 degrees
+  if (fifthRotation === 360) {
+    // 360 means rotate back to 0
+    fifthRotation = 0;
+  }
+
+  document.querySelector(
+    "#svg5"
+  ).style.transform = `rotate(${fifthRotation}deg)`;
+};
